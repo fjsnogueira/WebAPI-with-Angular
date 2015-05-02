f23e6b48276eb0cf5b913476da5467566b3c48a6
Dan Fernandez <dan@danfernandez.me>
2015-05-02 14:51:45 -0700

    Initial commit

---
+++ src/AngularClient/wwwroot/app/products/productListCtrl.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('productListCtrl', productListCtrl);
+
+    productListCtrl.$inject = ['$location']; 
+
+    function productListCtrl($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'productListCtrl';
+
+        activate();
+
+        function activate() { }
+    }
+})();
